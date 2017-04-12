24282da0ce14f48cface438f14bfa72bbd37bdc5
Shawn Pan <shawnpan@alum.mit.edu>
2017-04-12 11:55:55 -0400

    Adding ability to draw mask as well as polygon--useful for debugging.

--- Packages/sake/client/segment.js
+++ Packages/sake/client/segment.js
@@ -50,17 +50,26 @@ import { Viewerbase } from 'meteor/ohif:viewerbase';
         var imageMetadata = OHIF.viewer.metadataProvider.getMetadata(currentImageId);
 
         console.log("Getting segmentation from " + url);
-        console.log("Image id: " + currentImageId);
+        console.log("Image Metadata")
         console.log(imageMetadata);
 
-        $.ajax({
-          url: url,
-          data: {
+        //TODO handle missing metadata?
+        var requestData = {
             x: Math.round(x),
             y: Math.round(y),
             z: currentImageIdIndex,
+            patientName: imageMetadata.patient.name,
+            seriesInstanceUid: imageMetadata.series.seriesInstanceUid,
+            sopInstanceUid: imageMetadata.instance.sopInstanceUid,
             imageId: currentImageId
-          }
+        };
+        console.log("GET request parameters")
+        console.log(requestData)
+
+
+        $.ajax({
+          url: url,
+          data: requestData
         }).done(function(data) {
             console.log("ajax get request returned");
             console.log(data);
@@ -68,6 +77,19 @@ import { Viewerbase } from 'meteor/ohif:viewerbase';
             cornerstone.updateImage(mouseEventData.element);
         }).fail(function() {
             console.log("ajax get request failed");
+            console.log("dummy data!");
+            measurementData.segmentation = {
+                mask: [
+                    [1, 0, 0 ,0 ,1],
+                    [0, 1, 0 ,1 ,0],
+                    [0, 0, 1 ,0 ,0],
+                    [0, 1, 0 ,1 ,0],
+                    [1, 0, 0 ,0 ,1],
+                ],
+                maskOffset: [x - 2, y - 2],
+                polygon: [[x, y-7], [x+3, y-3], [x+4, y], [x+3, y+7], [x, y+7], [x-3, y+3], [x-4, y], [x-3, y-7]]
+            };
+            cornerstone.updateImage(mouseEventData.element);
         });
 
         return measurementData;
@@ -84,13 +106,20 @@ import { Viewerbase } from 'meteor/ohif:viewerbase';
     function onImageRendered(e, eventData) {
 
         // if we have no toolData for this element, return immediately as there is nothing to do
+        // console.log("current target");
+        // console.log(e.currentTarget);
         var toolData = cornerstoneTools.getToolState(e.currentTarget, toolType);
         if (!toolData) {
             return;
         }
 
         // we have tool data for this element - iterate over each one and draw it
-        var context = eventData.canvasContext.canvas.getContext('2d');
+        var canvas = eventData.canvasContext.canvas;
+        var canvasWidth = canvas.width;
+        var canvasHeight = canvas.height;
+        var context = canvas.getContext('2d');
+
+        //var context = eventData.canvasContext.canvas.getContext('2d');
         context.setTransform(1, 0, 0, 1, 0, 0);
 
         for (var i = 0; i < toolData.data.length; i++) {
@@ -101,6 +130,7 @@ import { Viewerbase } from 'meteor/ohif:viewerbase';
             var canvasPoint = cornerstone.pixelToCanvas(eventData.element, data.handles.start);
 
             context.fillStyle = color;
+            context.strokeStyle = color;
             context.beginPath();
             //(x, y, radiusX, radiusY, rotation, startAngle, endAngle, [anticlockwise]);
             context.ellipse(canvasPoint.x, canvasPoint.y, 2, 2, 0, 0, 2 * Math.PI)
@@ -111,15 +141,42 @@ import { Viewerbase } from 'meteor/ohif:viewerbase';
                 var pixelPairToCanvas = function(pair) {
                     return cornerstone.pixelToCanvas(eventData.element, {x: pair[0], y: pair[1]});
                 }
-                //draw polygon
-                context.beginPath();
-                var point = pixelPairToCanvas(data.segmentation[data.segmentation.length - 1]);
-                context.moveTo(point.x, point.y);
-                for (var j = 0; j < data.segmentation.length; j++) {
-                    point = pixelPairToCanvas(data.segmentation[j]);
-                    context.lineTo(point.x, point.y);
+                if (data.segmentation.polygon) {
+                    console.log("Drawing polygon");
+                    //draw polygon
+                    context.beginPath();
+                    var point = pixelPairToCanvas(data.segmentation.polygon[data.segmentation.polygon.length - 1]);
+                    context.moveTo(point.x, point.y);
+                    for (var j = 0; j < data.segmentation.polygon.length; j++) {
+                        point = pixelPairToCanvas(data.segmentation.polygon[j]);
+                        context.lineTo(point.x, point.y);
+                    }
+                    context.stroke();
                 }
-                context.fill();
+                console.log(data.segmentation.mask);
+                console.log(data.segmentation.maskOffset);
+                if (data.segmentation.mask && data.segmentation.maskOffset) {
+                    console.log("Drawing mask");
+                    //draw mask
+                    var xOffset = data.segmentation.maskOffset[0];
+                    var yOffset = data.segmentation.maskOffset[1];
+
+                    context.save();
+                    context.fillStyle = "#FF0000";
+
+                    for (var xMask = 0; xMask < data.segmentation.mask.length; xMask++) {
+                        for (var yMask = 0; yMask < data.segmentation.mask[0].length; yMask++) {
+                            var point = pixelPairToCanvas([xMask + xOffset, yMask + yOffset]);
+                            if (data.segmentation.mask[xMask][yMask]) {
+                                //draw pixel
+                                context.fillRect(point.x, point.y, 1, 1);
+                            }
+                        }
+                    }
+
+                    context.restore();
+
+                }
             }
 
             context.restore();
